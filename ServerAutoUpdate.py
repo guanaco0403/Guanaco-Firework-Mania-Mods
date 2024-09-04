fe41c61ebb49fa239e2ecfefaf4afee42e4c58c3
guanaco0403 <76651037+guanaco0403@users.noreply.github.com>
2024-09-04 11:47:14 +0200

    Update ServerAutoUpdate.py

--- ServerAutoUpdate.py
+++ ServerAutoUpdate.py
@@ -69,7 +69,7 @@ def main(github_token):
                     download_asset(asset, asset.name, github_token)
                     extract_zip(asset.name)
                     print("\u001B[32m=================================================\u001B[0m")
-                    print("  \u001B[36mFireworks Mania Server Successfully Installed\u001B[0m"")
+                    print("  \u001B[36mFireworks Mania Server Successfully Installed\u001B[0m")
                     print("\u001B[32m=================================================\u001B[0m")
                     return
         print("\u001B[31mNo matching asset found.\u001B[0m")
